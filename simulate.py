fccf4004a652e235c5c355b868336526ad45173c
A-Hoang1 <112354136+A-Hoang1@users.noreply.github.com>
2025-01-19 22:46:51 -0500

    print steps and make it loop through 1000

--- simulate.py
+++ simulate.py
@@ -1,4 +1,15 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+# disabling the sidebar
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+# simulate the world
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+
 p.disconnect()
