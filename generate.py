a904e5be6b3e1a2adb1471ada42020adf5211377
A-Hoang1 <112354136+A-Hoang1@users.noreply.github.com>
2025-01-27 18:10:08 -0500

    finished manylinks

--- generate.py
+++ generate.py
@@ -1,4 +1,46 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+# SDF file
+pyrosim.Start_SDF("boxes.sdf")
+
+# rows, columns
+rows = 5
+cols = 5
+
+# blocks
+blocks = 10
+
+# shrink
+shrink = 0.9
+
+# dimensions
+start_length = 1
+start_width: int = 1
+start_height = 1
+
+# position for Box
+x = 0
+y = 0
+z = start_height/2
+
+# spacing
+x_space = 1
+y_space = 1
+
+for row in range(rows):
+    for col in range(cols):
+        length = start_length
+        width = start_width
+        height = start_height
+
+        x = col * x_space
+        y = row * y_space
+        z = height/2
+
+        for i in range(blocks):
+            pyrosim.Send_Cube(name=f"Box_{i}", pos=[x,y,z], size=[length,width,height])
+            z += height
+            length *= shrink
+            width *= shrink
+            height *= shrink
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
+import pybullet_data
 import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-# disabling the sidebar
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.setGravity(0, 0, -9.8)
+planeID = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 # simulate the world
 for i in range(1000):
