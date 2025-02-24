53e6983373dd4e49601d0e030c633cc01a2e417c
A-Hoang1 <112354136+A-Hoang1@users.noreply.github.com>
2025-02-24 10:53:56 -0500

    Add files via upload

--- analyze.py
+++ analyze.py
@@ -1,19 +1,46 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# # Load sensor data
+# backLegSensorValues = np.load("data/backLegSensorValues.npy")
+# frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+
+# # Print the loaded values
+# print("Back Leg Sensor Values:", backLegSensorValues)
+# print("Front Leg Sensor Values:", frontLegSensorValues)
+
+# # Plot sensor values
+# plt.plot(backLegSensorValues, label="Back Leg Sensor", linewidth=1, linestyle='solid', color='purple')
+# plt.plot(frontLegSensorValues, label="Front Leg Sensor", linewidth=2, linestyle='dashed', color='gold')
+# plt.xlabel("Time Step")
+# plt.ylabel("Touch Sensor Value")
+# plt.title("BackLeg & FrontLeg Touch Sensor Data")
+# plt.legend()
+# plt.show()
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# targetAngles = np.load("data/targetAngles.npy")
+# plt.plot(targetAngles, marker='o', label="Motor Angles")
+# plt.xlabel("Index")
+# plt.ylabel("Angle (radians)")
+# plt.title("Generated Motor Angles")
+# plt.legend()
+# plt.show()
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load sensor data
-backLegSensorValues = np.load("data/backLegSensorValues.npy")
-frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+# Suppose you saved these arrays from simulate.py:
+backLegAngles  = np.load("data/backLegMotorCommands.npy")
+frontLegAngles = np.load("data/frontLegMotorCommands.npy")
 
-# Print the loaded values
-print("Back Leg Sensor Values:", backLegSensorValues)
-print("Front Leg Sensor Values:", frontLegSensorValues)
-
-# Plot sensor values
-plt.plot(backLegSensorValues, label="Back Leg Sensor", linewidth=1, linestyle='solid', color='purple')
-plt.plot(frontLegSensorValues, label="Front Leg Sensor", linewidth=2, linestyle='dashed', color='gold')
+plt.plot(backLegAngles,  label="Back Leg",  color="red")
+plt.plot(frontLegAngles, label="Front Leg", color="blue")
+plt.title("Motor Commands for Back & Front Legs")
 plt.xlabel("Time Step")
-plt.ylabel("Touch Sensor Value")
-plt.title("BackLeg & FrontLeg Touch Sensor Data")
+plt.ylabel("Angle (radians)")
 plt.legend()
 plt.show()
+
--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import time
 import pyrosim.pyrosim as pyrosim
 import numpy
 import os
+import random
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
@@ -15,15 +16,55 @@ p.loadSDF("world.sdf")
 robotID = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(robotID)
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
+steps = 1000
+backLegSensorValues = numpy.zeros(steps)
+frontLegSensorValues = numpy.zeros(steps)
+
+xVals = numpy.linspace(0, 2*numpy.pi, steps)
+rawSinVals = numpy.sin(xVals)
+targetAngles = rawSinVals * (numpy.pi/4.0)
+
+amplitude_back = numpy.pi/2.5
+freq_back = 20
+phaseOffset_back = -20
+
+amplitude_front = numpy.pi/2.5
+freq_front = 20.0
+phaseOffset_front = 15
+
+backLegMotorCommands = amplitude_back * numpy.sin(freq_back *xVals + phaseOffset_back)
+frontLegMotorCommands = amplitude_front * numpy.sin(freq_front *xVals + phaseOffset_front)
+# numpy.save("C:/Users/2678d/PycharmProjects/CS3060bots/data/backLegMotorCommands.npy", backLegMotorCommands)
+# numpy.save("C:/Users/2678d/PycharmProjects/CS3060bots/data/frontLegMotorCommands.npy", frontLegMotorCommands)
+# exit()
+
 
 # simulate the world
-for i in range(1000):
+for i in range(steps):
     p.stepSimulation()
     time.sleep(1/60)
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    randomBack_Angle = random.random()*numpy.pi - numpy.pi/2
+    randomFront_Angle = random.random()*numpy.pi - numpy.pi/2
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex    = robotID,
+        jointName    = b'Torso_BackLeg',   
+        controlMode  = p.POSITION_CONTROL,
+        targetPosition = backLegMotorCommands[i],            
+        maxForce     = 20                 
+    )
+
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex      = robotID,
+    jointName      = b'Torso_FrontLeg',
+    controlMode    = p.POSITION_CONTROL,
+    targetPosition = frontLegMotorCommands[i],
+    maxForce       = 20
+    )
+
 
 p.disconnect()
 
